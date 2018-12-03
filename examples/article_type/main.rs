adb6916f937ec3f973a41f50d35dddcfe71b3b6f
John-Paul Robinson <jprorama@gmail.com>
2018-12-03 12:35:56 -0600

    Add README.md article type id to example code dir
    Moved default example to parse_dump subdir
    Create new example based on parse_dump with type id documented in README.md
    Remove example section from Cargo.toml to build all examples

---
+++ examples/article_type/main.rs
@@ -0,0 +1,55 @@
+// Copyright 2018 Fredrik Portström <https://portstrom.com>
+// This is free software distributed under the terms specified in
+// the file LICENSE at the top-level directory of this distribution.
+
+extern crate bzip2;
+extern crate parse_mediawiki_dump;
+
+fn main() {
+    let mut args = std::env::args();
+    if args.len() != 2 {
+        eprintln!("invalid use");
+        std::process::exit(1);
+    }
+    let path = args.nth(1).unwrap();
+    let file = match std::fs::File::open(&path) {
+        Err(error) => {
+            eprintln!("Failed to open input file: {}", error);
+            std::process::exit(1);
+        }
+        Ok(file) => std::io::BufReader::new(file),
+    };
+    if path.ends_with(".bz2") {
+        parse(std::io::BufReader::new(bzip2::bufread::BzDecoder::new(
+            file,
+        )));
+    } else {
+        parse(file);
+    }
+}
+
+fn parse(source: impl std::io::BufRead) {
+    for result in parse_mediawiki_dump::parse(source) {
+        match result {
+            Err(error) => {
+                eprintln!("Error: {}", error);
+                std::process::exit(1);
+            }
+            Ok(page) => if page.namespace == 0 && match &page.format {
+                None => false,
+                Some(format) => format == "text/x-wiki"
+            } && match &page.model {
+                None => false,
+                Some(model) => model == "wikitext"
+            } {
+                println!(
+                    "The page {title:?} is an ordinary article with byte length {length}.",
+                    title = page.title,
+                    length = page.text.len()
+                );
+            } else {
+                println!("The page {:?} has something special to it.", page.title);
+            }
+        }
+    }
+}
